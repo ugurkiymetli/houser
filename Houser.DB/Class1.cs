f291c77983eacd7020c44a3d9c92b12d8fca148a
ugurkiymetli <34272634+ugurkiymetli@users.noreply.github.com>
2021-12-28 19:58:29 +0300

    Houser init.

---
+++ Houser.DB/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Houser.DB
+{
+    public class Class1
+    {
+    }
+}
---
+++ Houser.Service/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Houser.Service
+{
+    public class Class1
+    {
+    }
+}
