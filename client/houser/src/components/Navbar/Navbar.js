e94716f724738c1430f3aba34a3b4c3ed438fe47
ugurkiymetli <34272634+ugurkiymetli@users.noreply.github.com>
2022-01-12 19:58:50 +0300

    Navbar Icons updated.
    React-Icons added.

--- client/houser/src/components/Navbar/Navbar.js
+++ client/houser/src/components/Navbar/Navbar.js
@@ -1,25 +1,23 @@
-import { Box, Flex, Avatar, Button, Stack } from "@chakra-ui/react";
+import { Box, Flex, Button, Stack } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
-import {
-  CalendarIcon,
-  ChatIcon,
-  DragHandleIcon,
-  ViewIcon,
-  StarIcon,
-  CloseIcon,
-} from "@chakra-ui/icons";
+
+import { FiUsers, FiUser } from "react-icons/fi";
+import { MdOutlinePayments, MdLogout, MdApartment } from "react-icons/md";
+import { TiMessages } from "react-icons/ti";
+import { IoIosRocket } from "react-icons/io";
 import { useAuth } from "../../context/AuthContext";
+import styles from "./styles.module.css";
 function Navbar() {
   const { user, isLoggedIn, isAdmin, logout } = useAuth();
 
   return (
     <>
-      <Box bg={"gray.100"} px={4}>
+      <Box bg={"gray.100"} px={4} className={styles.nav}>
         <Flex h={16} alignItems={"center"} justifyContent={"space-between"}>
-          <Box>
+          <Box className={styles.hero}>
             <Button variant="Link" fontSize={"lg"} href={"#"}>
-              <StarIcon mr={1} />
-              Houser
+              <IoIosRocket />
+              Houser{isAdmin ? " - Admin" : null}
             </Button>
           </Box>
           {/* if user is not logged in */}
@@ -31,12 +29,12 @@ function Navbar() {
               spacing={6}
             >
               <Link to="/register">
-                <Button variant="solid" colorScheme="blue" href={"#"}>
+                <Button variant="solid" colorScheme="gray" href={"#"}>
                   Sign In
                 </Button>
               </Link>
               <Link to="/login">
-                <Button variant="solid" colorScheme="blue" href={"#"}>
+                <Button variant="solid" colorScheme="gray" href={"#"}>
                   Sign Up
                 </Button>
               </Link>
@@ -51,13 +49,13 @@ function Navbar() {
                   <>
                     <Link to="/apartments">
                       <Button variant="outline" colorScheme="black" href={"#"}>
-                        <DragHandleIcon mr={1} />
+                        <MdApartment />
                         Apartments
                       </Button>
                     </Link>
                     <Link to="/users">
                       <Button variant="outline" colorScheme="black" href={"#"}>
-                        <ViewIcon mr={1} />
+                        <FiUsers />
                         Users
                       </Button>
                     </Link>
@@ -65,29 +63,24 @@ function Navbar() {
                 )}
                 <Link to="/payments">
                   <Button variant="outline" colorScheme="black" href={"#"}>
-                    <CalendarIcon mr={1} />
+                    <MdOutlinePayments />
                     Payments
                   </Button>
                 </Link>
                 <Link to="/messages">
                   <Button variant="outline" colorScheme="black" href={"#"}>
-                    <ChatIcon mr={1} />
+                    <TiMessages />
                     Messages
                   </Button>
                 </Link>
                 <Link to="/profile">
-                  <Button variant="outline" colorScheme="black" href={"#"}>
-                    <Avatar
-                      size={"sm"}
-                      src={"https://avatars.dicebear.com/api/male/username.svg"}
-                      mr={1}
-                    />
-                    {user.name}
+                  <Button variant="ghost" colorScheme="black" mr={0} href={"#"}>
+                    <FiUser />
+                    {user.name.split(" ")[0]}
                   </Button>
                 </Link>
-                <Button colorScheme="red" onClick={logout}>
-                  Logout
-                  <CloseIcon boxSize="3" ml="2" />
+                <Button colorScheme="red" variant="ghost" onClick={logout}>
+                  <MdLogout style={{ fontSize: "1.5rem" }} />
                 </Button>
               </Stack>
             </Flex>
